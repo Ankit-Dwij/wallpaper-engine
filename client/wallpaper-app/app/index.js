b15343b8735dd9c0e5d8538cdbca5c8123057992
Ankit Dwij <ankitdwiz010@gmail.com>
2024-07-12 21:37:46 +0530

    client(homescreen): added homescreen and animations

---
+++ client/wallpaper-app/app/index.js
@@ -0,0 +1,86 @@
+import { View, Text, StyleSheet, Image, Pressable } from "react-native";
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { hp, wp } from "../helpers/common";
+import { LinearGradient } from "expo-linear-gradient";
+import Animated, { FadeInDown } from "react-native-reanimated";
+import { theme } from "../constants/theme";
+
+const WelcomeScreen = () => {
+  return (
+    <View style={styles.container}>
+      <StatusBar style="light" />
+      <Image
+        source={require("../assets/images/welcome.png")}
+        style={styles.bgImage}
+        resizeMode="cover"
+      />
+      {/* linear gradient */}
+      <Animated.View entering={FadeInDown.duration(600)} style={{ flex: 1 }}>
+        <LinearGradient
+          colors={[
+            "rgba(255,255,255,0)",
+            "rgba(255,255,255,0.5)",
+            "white",
+            "white",
+          ]}
+          style={styles.gradient}
+          start={{ x: 0.5, y: 0 }}
+          end={{ x: 0.5, y: 0.8 }}
+        />
+        {/* content */}
+        <View style={styles.contentContainer}>
+          <Animated.Text
+            entering={FadeInDown.delay(400).springify()}
+            style={styles.title}
+          >
+            WallE
+          </Animated.Text>
+          <Animated.Text
+            entering={FadeInDown.delay(500).springify()}
+            style={styles.punchline}
+          >
+            Revamp your screen, refresh your vibe!
+          </Animated.Text>
+
+          <Animated.View entering={FadeInDown.delay(600).springify()}>
+            <Pressable style={styles.startButton}>
+              <Text style={styles.startText}>Start Exploring</Text>
+            </Pressable>
+          </Animated.View>
+        </View>
+      </Animated.View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: { flex: 1 },
+  bgImage: { width: wp(100), height: hp(100), position: "absolute" },
+  gradient: { width: wp(100), height: hp(65), position: "absolute", bottom: 0 },
+  contentContainer: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "flex-end",
+    gap: 14,
+  },
+  title: {
+    fontSize: hp(7),
+    color: theme.colors.neutral(0.9),
+    fontWeight: theme.fontWeights.bold,
+  },
+  punchline: {
+    fontSize: hp(2),
+    marginBottom: 10,
+  },
+  startButton: {
+    marginBottom: 50,
+    backgroundColor: theme.colors.neutral(0.9),
+    padding: 15,
+    paddingHorizontal: 70,
+    borderRadius: theme.radius.xl,
+  },
+  startText: { color: theme.colors.white, fontSize: hp(3), letterSpacing: 1 },
+});
+
+export default WelcomeScreen;
